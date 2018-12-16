1a066e9409a19588894d8815d58331192d8f9849
3a532008 <isme1788@gmail.com>
2018-12-16 16:12:27 +0800

    AdminPostsController更正

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -10,7 +10,6 @@ class AdminPostsController extends Controller
 {
     public function index()
     {
-        return view('admin.posts.index');
         $posts=Post::orderBy('created_at','DESC')->get();
         $data=['posts'=>$posts];
         return view('admin.posts.index',$data);
